0d0f07b53f2fafaa9758af420589bdcfe8f6fe0c
fespinomartinez507-star <fespinomartinez507@gmail.com>
2026-05-27 18:56:56 -0500

    save

---
+++ Lab8/Lab813.py
@@ -0,0 +1,15 @@
+class Externa:
+    def __init__(self):
+        self.name = "Externa"
+
+    class Interna:
+        def __init__(self):
+            self.name = "Interna"
+
+        def display(self):
+            print("Hola desde la clase interna")
+
+
+ext = Externa()
+int_obj = ext.Interna()
+int_obj.display()
